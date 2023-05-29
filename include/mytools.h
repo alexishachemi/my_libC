8b1fdaf82b4b02719dfe5328b1ef0d86372e53ed
alexis.hachemi <alexis.hachemi@epitech.eu>
2023-05-29 10:23:30 +0200

    Update mytools macro

--- include/mytools.h
+++ include/mytools.h
@@ -8,13 +8,19 @@
 #ifndef MYTOOLS_H_
     #define MYTOOLS_H_
 
+    #define __GET_MACRO__(_1, _2, NAME, ...) NAME
     #define UNUSED __attribute__((unused))
-    #define ASSERT(x, err) if (!(x)) return err
+    #define ASSERT_VOID(x) if (!(x)) return
+    #define ASSERT_VAL(x, err) if (!(x)) return err
+    #define ASSERT(...) __GET_MACRO__(__VA_ARGS__, ASSERT_VAL, ASSERT_VOID)\
+    (__VA_ARGS__)
     #define SUCCESS 0
-    #define FAILURE 84
+    #define FAILURE -1
+    #define EXIT_F 84
+    #define EXIT_S 0
 
-    #define MIN(n, min) (n < min ? min : n)
-    #define MAX(n, max) (n > max ? max : n)
+    #define MIN(n, min) (n <= min ? min : n)
+    #define MAX(n, max) (n >= max ? max : n)
     #define CLAMP(n, min, max) (MAX(MIN(n, min), max))
     #define IS_BETWEEN(n, min, max) (n >= min && n <= max)
 
